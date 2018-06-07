f9b84c64323f4d8da3027d41c2a578a80a3ebc3c
Mikhail Lysyansky <mikhail.lysyansky@jetruby.com>
2018-06-07 12:19:44 +0300

    Draw text, lines, polygons.

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -10,7 +10,16 @@ import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.PolygonOptions;
+import com.baidu.mapapi.map.Polyline;
+import com.baidu.mapapi.map.PolylineOptions;
+import com.baidu.mapapi.map.Stroke;
+import com.baidu.mapapi.map.TextOptions;
 import com.baidu.mapapi.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         mMapView = findViewById(R.id.map_view);
         initMapStatus(new LatLng(39.5088582,115.1981377));
         initOverlay(new LatLng(39.5088582,115.1981377));
+//      drawLine();
+//      drawPolygon();
+//      addText();
     }
 
     private void initMapStatus(LatLng latLng) {
@@ -47,6 +59,55 @@ public class MainActivity extends AppCompatActivity {
         marker.setDraggable(false);
     }
 
+    private void drawLine(){
+        LatLng p1 = new LatLng(39.97923, 116.357428);
+        LatLng p2 = new LatLng(39.94923, 116.397428);
+        LatLng p3 = new LatLng(39.97923, 116.437428);
+        List<LatLng> points = new ArrayList<>();
+        points.add(p1);
+        points.add(p2);
+        points.add(p3);
+
+        OverlayOptions ooPolyline = new PolylineOptions().width(10)
+                .color(0xAAFF0000).points(points);
+        Polyline mPolyline = (Polyline) mBaiduMap.addOverlay(ooPolyline);
+        mPolyline.setDottedLine(true);
+    }
+
+    private void drawPolygon(){
+        LatLng pt1 = new LatLng(39.93923, 116.357428);
+        LatLng pt2 = new LatLng(39.91923, 116.327428);
+        LatLng pt3 = new LatLng(39.89923, 116.347428);
+        LatLng pt4 = new LatLng(39.89923, 116.367428);
+        LatLng pt5 = new LatLng(39.91923, 116.387428);
+        List<LatLng> pts = new ArrayList<LatLng>();
+        pts.add(pt1);
+        pts.add(pt2);
+        pts.add(pt3);
+        pts.add(pt4);
+        pts.add(pt5);
+
+        OverlayOptions polygonOption = new PolygonOptions()
+                .points(pts)
+                .stroke(new Stroke(5, 0xAA00FF00))
+                .fillColor(0xAAFFFF00);
+
+        mBaiduMap.addOverlay(polygonOption);
+    }
+
+    private void addText() {
+        LatLng llText = new LatLng(39.86923, 116.397428);
+
+        OverlayOptions textOption = new TextOptions()
+                .bgColor(0xAAFFFF00)
+                .fontSize(24)
+                .fontColor(0xFFFF00FF)
+                .text("YOUR TEXT MARKER")
+                .rotate(-30)
+                .position(llText);
+
+        mBaiduMap.addOverlay(textOption);
+    }
 
     @Override
     protected void onResume() {
